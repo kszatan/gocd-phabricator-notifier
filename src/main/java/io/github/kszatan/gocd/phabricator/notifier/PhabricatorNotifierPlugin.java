41a83ff6994b4e18db9ce9069215311ec3a7d948
Krzysztof Szatan <kszatan@gmail.com>
2017-06-14 12:14:20 +0200

    Plugin scaffold

---
+++ src/main/java/io/github/kszatan/gocd/phabricator/notifier/PhabricatorNotifierPlugin.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2017 Krzysztof Szatan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.kszatan.gocd.phabricator.notifier;
+
+import com.thoughtworks.go.plugin.api.AbstractGoPlugin;
+import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
+import com.thoughtworks.go.plugin.api.annotation.Extension;
+import com.thoughtworks.go.plugin.api.exceptions.UnhandledRequestTypeException;
+import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
+import com.thoughtworks.go.plugin.api.response.DefaultGoPluginApiResponse;
+import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+import io.github.kszatan.gocd.phabricator.notifier.handlers.RequestHandler;
+import io.github.kszatan.gocd.phabricator.notifier.handlers.RequestHandlerFactory;
+
+import java.util.Collections;
+import java.util.List;
+
+@Extension
+public class PhabricatorNotifierPlugin extends AbstractGoPlugin {
+    private static final String EXTENSION_NAME = "notification";
+    private static final List<String> supportedExtensionVersions = Collections.singletonList("1.0");
+    private final RequestHandlerFactory requestHandlerFactory;
+    public PhabricatorNotifierPlugin() {
+        requestHandlerFactory = null;
+    }
+
+    public PhabricatorNotifierPlugin(RequestHandlerFactory requestHandlerFactory) {
+        this.requestHandlerFactory = requestHandlerFactory;
+    }
+
+    @Override
+    public GoPluginApiResponse handle(GoPluginApiRequest request) {
+        GoPluginApiResponse response;
+        try {
+            RequestHandler requestHandler = requestHandlerFactory.create(request.requestName());
+            response = requestHandler.handle(request);
+        } catch (UnhandledRequestTypeException e) {
+            response = DefaultGoPluginApiResponse.badRequest("Invalid request name: " + request.requestName());
+        } catch (Exception e) {
+            response = DefaultGoPluginApiResponse.error("Unknown error during request processing: " + e.getMessage());
+        }
+        return response;
+    }
+
+    @Override
+    public GoPluginIdentifier pluginIdentifier() {
+        return new GoPluginIdentifier(EXTENSION_NAME, supportedExtensionVersions);
+    }
+}
---
+++ src/test/java/io/github/kszatan/gocd/phabricator/notifier/PhabricatorNotifierPluginTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2017 Krzysztof Szatan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.kszatan.gocd.phabricator.notifier;
+
+import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
+import com.thoughtworks.go.plugin.api.exceptions.UnhandledRequestTypeException;
+import com.thoughtworks.go.plugin.api.request.DefaultGoPluginApiRequest;
+import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
+import com.thoughtworks.go.plugin.api.response.DefaultGoPluginApiResponse;
+import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+import io.github.kszatan.gocd.phabricator.notifier.handlers.RequestHandler;
+import io.github.kszatan.gocd.phabricator.notifier.handlers.RequestHandlerFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PhabricatorNotifierPluginTest {
+    private PhabricatorNotifierPlugin plugin;
+    private RequestHandlerFactory requestHandlerFactory;
+    private RequestHandler requestHandler;
+
+    @Before
+    public void setUp() throws Exception {
+        requestHandlerFactory = mock(RequestHandlerFactory.class);
+        plugin = new PhabricatorNotifierPlugin(requestHandlerFactory);
+        requestHandler = mock(RequestHandler.class);
+    }
+
+    @Test
+    public void handleShouldReturnBadRequestResponseInCaseOfUnhandledRequestTypeException() throws Exception {
+        when(requestHandlerFactory.create(anyString())).thenThrow(UnhandledRequestTypeException.class);
+
+        GoPluginApiRequest request = new DefaultGoPluginApiRequest("notification", "1.0", "unhandled-request");
+        GoPluginApiResponse response = plugin.handle(request);
+
+        assertThat(response.responseCode(), equalTo(DefaultGoPluginApiResponse.BAD_REQUEST));
+    }
+
+    @Test
+    public void handleShouldReturnErrorResponseInCaseOfUnknownException() throws Exception {
+        when(requestHandlerFactory.create(anyString())).thenThrow(Exception.class);
+
+        GoPluginApiRequest request = new DefaultGoPluginApiRequest("notification", "1.0", RequestHandlerFactory.NOTIFICATIONS_INTERESTED_IN);
+        GoPluginApiResponse response = plugin.handle(request);
+
+        assertThat(response.responseCode(), equalTo(DefaultGoPluginApiResponse.INTERNAL_ERROR));
+    }
+
+    @Test
+    public void handleShouldReturnOriginalResponseInAbsenceOfExceptions() throws Exception {
+        GoPluginApiResponse preparedResponse = DefaultGoPluginApiResponse.success("Body");
+        GoPluginApiRequest request = new DefaultGoPluginApiRequest("notification", "1.0", RequestHandlerFactory.NOTIFICATIONS_INTERESTED_IN);
+        when(requestHandler.handle(any(GoPluginApiRequest.class))).thenReturn(preparedResponse);
+        when(requestHandlerFactory.create(anyString())).thenReturn(requestHandler);
+
+        GoPluginApiResponse returnedResponse = plugin.handle(request);
+
+        assertThat(returnedResponse, equalTo(preparedResponse));
+    }
+
+    @Test
+    public void pluginIdentifierShouldReturnCorrectPluginInfo() {
+        PhabricatorNotifierPlugin plugin = new PhabricatorNotifierPlugin();
+        GoPluginIdentifier identifier = plugin.pluginIdentifier();
+        assertNotNull(identifier);
+        assertEquals("invalid type of extension", "notification", identifier.getExtension());
+    }
+}
